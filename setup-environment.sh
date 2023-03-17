f1bb2f12e18e974e1f746e8087b1c09ccb0006ae
gustavodelima <ghtp25@gmail.com>
2023-03-17 20:21:05 -0300

    setup environment script

---
+++ setup-environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Installing minikube
+sudo apt-get update && sudo apt-get install -y curl virtualbox
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+
+# Installing kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
