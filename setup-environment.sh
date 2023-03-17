a3f45c97270f301d43d4943c5590d2b0ff617a19
gustavodelima <ghtp25@gmail.com>
2023-03-17 20:28:14 -0300

    updating setup environment script with helm and argo-cd

--- setup-environment.sh
+++ setup-environment.sh
@@ -12,4 +12,14 @@ sudo chmod +x /usr/local/bin/minikube
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# Installing Helm
+sudo snap install helm --classic
+
+# Installing argo-cd
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd
+
+kubectl port-forward svc/argocd-server -n default 8080:443
+kubectl -n default get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
 
